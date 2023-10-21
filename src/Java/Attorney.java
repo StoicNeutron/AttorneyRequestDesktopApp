e38c112a6acc7cfd657ec5f0edccbde18e90c596
nleaptro-gmu <nleaptro@gmu.edu>
2023-10-20 23:13:00 -0400

    First commit

---
+++ src/Java/Attorney.java
@@ -0,0 +1,39 @@
+package GroupProject;
+
+import java.util.LinkedList;
+
+public class Attorney {
+    private String name;
+    private String contactInfo;
+    private LinkedList<Immigrant> clients;
+
+    public Attorney() {
+        this.name = "Mr. Law";
+        this.contactInfo = "(703) 123-1234";
+        this.clients = new LinkedList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInfo() {
+        return this.contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public LinkedList<Immigrant> getClients() {
+        return this.clients;
+    }
+
+    public void addClient(Immigrant immigrant) {
+        clients.add(immigrant);
+    }
+}
---
+++ src/test/ImmigrantTest.java
@@ -0,0 +1,30 @@
+package GroupProject;
+import static org.junit.Assert.*;
+
+import java.time.LocalDateTime;
+
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
+public class ImmigrantTest {
+    @Test
+    @SuppressWarnings("deprecation")
+   public void test()
+   {
+      Immigrant alien = new Immigrant();
+      assertNotNull(alien);
+      assertThat(alien.getName(), is("John Doe"));
+      assertThat(alien.getANumber(), is(12345));
+      assertThat(alien.getDOB().toString(), is("2023-04-24T14:33"));
+      assertThat(alien.getAddress(), is("1234 Fillername Street"));
+      //now test setters
+      alien.setName("Jane Doe");
+      alien.setANumber(98765);
+      alien.setDOB(LocalDateTime.of(1984, 03, 12, 04, 03, 00));
+      alien.setAddress("Nowhere, Notaplace");
+      assertThat(alien.getName(), is("Jane Doe"));
+      assertThat(alien.getANumber(), is(98765));
+      assertThat(alien.getDOB().toString(), is("1984-03-12T04:03"));
+      assertThat(alien.getAddress(), is("Nowhere, Notaplace"));
+
+   }
+}
